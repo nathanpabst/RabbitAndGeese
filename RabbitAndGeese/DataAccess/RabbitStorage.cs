bd214f34b79702713c124846b9fa99ed74c6370b
Nathan Pabst <nathan.pabst@gmail.com>
2018-09-18 19:45:48 -0500

    fixed error

--- RabbitAndGeese/DataAccess/RabbitStorage.cs
+++ RabbitAndGeese/DataAccess/RabbitStorage.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using RabbitAndGeese.Models;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
@@ -7,5 +8,12 @@ namespace RabbitAndGeese.DataAccess
 {
     public class RabbitStorage
     {
+        static List<Rabbit> _hutch;
+
+
+        public void Add(Rabbit rabbit)
+        {
+            _hutch.Add(rabbit); 
+        }
     }
 }
