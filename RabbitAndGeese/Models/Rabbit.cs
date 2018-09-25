3f31066ef46f22f02428a3a9695913b5a58beff3
Nathan Pabst <nathan.pabst@gmail.com>
2018-09-25 16:59:56 -0500

    wip

--- RabbitAndGeese/Models/Rabbit.cs
+++ RabbitAndGeese/Models/Rabbit.cs
@@ -1,8 +1,5 @@
-﻿using RabbitAndGeese.Models;
-using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Threading.Tasks;
+﻿using System.Collections.Generic;
+
 
 namespace RabbitAndGeese.Models
 
